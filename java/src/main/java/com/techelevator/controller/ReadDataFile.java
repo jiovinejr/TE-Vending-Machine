93e96434d2ec44c9634cfff28da880bc631ae58e
James Iovine <jiovinejr@gmail.com>
2023-03-30 15:47:41 -0400

    jimmy messing around

--- java/src/main/java/com/techelevator/controller/ReadDataFile.java
+++ java/src/main/java/com/techelevator/controller/ReadDataFile.java
@@ -9,16 +9,15 @@ import java.util.List;
 public class ReadDataFile {
   //A2,Ginger Ayle,1.85,Drink
 
-   private String catering = "catering.txt";
-   private String catering1 = "catering1.txt";
+   private String catering = "catering.csv";
+   private String catering1 = "catering1.csv";
    private String dataPath = "";
 
     public List<Product> loadFile() {
         File dataFile = new File(catering);
-        List<Product> importedProductsList = new ArrayList<>();
+        List<Product> importedProducts = new ArrayList<>();
 
-
-        return importedProductsList;
+        return importedProducts;
     }
 
 }
